890bddc80644776d82b1c490f91d12558e611015
Martin Zuther <code@mzuther.de>
2015-02-28 13:18:45 +0100

    v1.9.0: lots of internal changes

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -333,13 +333,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  0
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                1.08.0
+ #define JucePlugin_Version                1.9.0
 #endif
 #ifndef  JucePlugin_VersionCode
- #define JucePlugin_VersionCode            0x10800
+ #define JucePlugin_VersionCode            0x10900
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "1.08.0"
+ #define JucePlugin_VersionString          "1.9.0"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -39,8 +39,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "traKmeter";
-    const char* const  versionString  = "1.08.0";
-    const int          versionNumber  = 0x10800;
+    const char* const  versionString  = "1.9.0";
+    const int          versionNumber  = 0x10900;
 }
 #endif
 
