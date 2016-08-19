7e9c1b7785ee27953656f7993d698cc595898708
Martin Zuther <code@mzuther.de>
2016-08-19 21:49:25 +0200

    updated libraries
    * JUCE 4.2.3
    
    * VST 3.6.6

--- Builds/premake5.lua
+++ Builds/premake5.lua
@@ -237,7 +237,7 @@ workspace "trakmeter"
 			}
 
 			includedirs {
-				"../libraries/asiosdk2.3/common"
+				"../libraries/asio/common"
 			}
 
 		filter { "configurations:Debug" }
@@ -285,7 +285,7 @@ workspace "trakmeter"
 			}
 
 			includedirs {
-				"../libraries/asiosdk2.3/common"
+				"../libraries/asio/common"
 			}
 
 		filter { "configurations:Debug" }
@@ -324,7 +324,7 @@ workspace "trakmeter"
 		}
 
 		includedirs {
-			"../libraries/vstsdk3.6.5"
+			"../libraries/vst"
 		}
 
 		filter { "system:linux" }
@@ -369,7 +369,7 @@ workspace "trakmeter"
 		}
 
 		includedirs {
-			"../libraries/vstsdk3.6.5"
+			"../libraries/vst"
 		}
 
 		filter { "system:linux" }
