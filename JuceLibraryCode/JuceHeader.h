e1b659dc60499f248887f0b2fa6f29bf55779f85
Martin Zuther <code@mzuther.de>
2018-01-23 22:17:41 +0100

    updated JCUE library to 5.1.2

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -10,8 +10,7 @@
 
 */
 
-#ifndef __APPHEADERFILE_PTVFE5__
-#define __APPHEADERFILE_PTVFE5__
+#pragma once
 
 #include "AppConfig.h"
 
@@ -45,5 +44,3 @@ namespace ProjectInfo
     const int          versionNumber  = 0x20300;
 }
 #endif
-
-#endif   // __APPHEADERFILE_PTVFE5__
