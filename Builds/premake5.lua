ee9a2575cc210c6f1f5aa20ebb2625e9fec9031c
Martin Zuther <code@mzuther.de>
2020-05-02 10:52:49 +0200

    upgrade to premake5 (alpha15)

--- Builds/premake5.lua
+++ Builds/premake5.lua
@@ -122,8 +122,6 @@ workspace "trakmeter"
             "_USE_MATH_DEFINES=1",
         }
 
-        systemversion "10.0.17763.0"
-
         flags {
            "MultiProcessorCompile",
            "NoMinimalRebuild"
